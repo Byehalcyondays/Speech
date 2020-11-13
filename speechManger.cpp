3f744ad576d3f215ff0e6e4a8f584b8f60427e4e
Byehalcyondays <1224702638@qq.com>
2020-11-13 17:11:40 +0800

    Initial commit

---
+++ speechManger.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Halcyon on 2020/11/13.
+//
+
+#include "speechManger.h"
+
+SpeecgManager::SpeecgManager()
+{
+
+}
+
+SpeecgManager::~SpeecgManager()
+{
+
+}
---
+++ speechManger.h
@@ -0,0 +1,23 @@
+//
+// Created by Halcyon on 2020/11/13.
+//
+#ifndef SPEECH_SPEECHMANGER_H
+#define SPEECH_SPEECHMANGER_H
+
+#include <iostream>
+using namespace std;
+
+class SpeecgManager{
+public:
+    //构造函数
+    SpeecgManager();
+    //析构函数
+    ~SpeecgManager();
+};
+
+
+
+
+
+#endif //SPEECH_SPEECHMANGER_H
+
